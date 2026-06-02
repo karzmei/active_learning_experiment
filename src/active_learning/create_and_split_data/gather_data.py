1e41dd37793c2008a6e9dfb8f89cd108bfcc982b
Karina Samvelyan <karina.samvelyan@gmail.com>
2026-06-02 11:11:26 +0200

    changing data format to relative paths and saving splits in one csv file

--- src/active_learning/create_and_split_data/gather_data.py
+++ src/active_learning/create_and_split_data/gather_data.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from torchvision.datasets import EuroSAT
 import pandas as pd
 
-from utils.constants import DATA_MAIN_DIR_PATH, DATA_FOLDERS_PATH, DATA_MAIN_CSV_PATH
+from src.active_learning.utils.constants import DATA_MAIN_DIR_PATH, DATA_FOLDERS_PATH, DATA_MAIN_CSV_PATH
 
 def download_data(data_dir_path: Path = DATA_MAIN_DIR_PATH):
     dataset = EuroSAT(
@@ -13,14 +13,15 @@ def download_data(data_dir_path: Path = DATA_MAIN_DIR_PATH):
 def create_main_data_table(data_dir_path: Path = DATA_FOLDERS_PATH, path_to_save_csv: Path = DATA_MAIN_CSV_PATH):
     classes_dir_paths = data_dir_path.iterdir()
 
-    paths = []
+    relative_paths = []
     labels = []
     for class_dir_path in classes_dir_paths:
         img_paths = list(class_dir_path.glob("*.jpg"))
-        paths.extend(img_paths)
-        labels.extend([class_dir_path.stem] * len(img_paths))
+        relative_img_paths = [img_path.relative_to(data_dir_path) for img_path in img_paths]
+        relative_paths.extend(relative_img_paths)
+        labels.extend([class_dir_path.stem] * len(relative_img_paths))
 
-    df = pd.DataFrame({"image_path": paths, "label": labels})
+    df = pd.DataFrame({"relative_path": relative_paths, "label": labels})
 
     df.to_csv(path_to_save_csv, index=False)
 
--- src/active_learning/create_and_split_data/split_data.py
+++ src/active_learning/create_and_split_data/split_data.py
@@ -2,37 +2,37 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 
-from src.active_learning.utils.constants import DATA_MAIN_CSV_PATH, INITIAL_REMAINING_POOL_SPLIT_SAVE_PATH, POOL_SPLIT_SAVE_PATH, TEST_SPLIT_SIZE, VALIDATION_SPLIT_SIZE, INITIAL_TRAINING_SPLIT_SAVE_PATH, TEST_SPLIT_SAVE_PATH, VALIDATION_SPLIT_SAVE_PATH
+from src.active_learning.utils.constants import DATA_MAIN_CSV_PATH, TEST_SPLIT_SIZE, VALIDATION_SPLIT_SIZE, BASE_DATA_SPLIT_CSV_PATH
 from src.active_learning.utils.constants import INITIAL_DATA_SPLIT_SEED
 
-def split_csv_test_validation_pool(data_csv_path=DATA_MAIN_CSV_PATH, test_size=TEST_SPLIT_SIZE, val_size=VALIDATION_SPLIT_SIZE, random_state=INITIAL_DATA_SPLIT_SEED, test_split_save_path=TEST_SPLIT_SAVE_PATH, validation_split_save_path=VALIDATION_SPLIT_SAVE_PATH, pool_split_save_path=POOL_SPLIT_SAVE_PATH):
+def split_csv_test_validation_pool(data_csv_path=DATA_MAIN_CSV_PATH, test_size=TEST_SPLIT_SIZE, val_size=VALIDATION_SPLIT_SIZE, random_state=INITIAL_DATA_SPLIT_SEED, base_data_split_save_path=BASE_DATA_SPLIT_CSV_PATH, ):
     data_df = pd.read_csv(data_csv_path)
 
     trainval_df, test_df = train_test_split(data_df, test_size=test_size, random_state=random_state, stratify=data_df["label"])
 
     pool_df, val_df = train_test_split(trainval_df, test_size=val_size, random_state=random_state, stratify=trainval_df["label"])
 
-    pool_split_save_path.parent.mkdir(parents=True, exist_ok=True)
-    validation_split_save_path.parent.mkdir(parents=True, exist_ok=True)
-    test_split_save_path.parent.mkdir(parents=True, exist_ok=True)
+    # create df with columns: relative_path, label, base_split (with values: "", "validation", "test")
 
-    pool_df.to_csv(pool_split_save_path, index=False)
-    val_df.to_csv(validation_split_save_path, index=False)
-    test_df.to_csv(test_split_save_path, index=False)
+    base_data_split_df = pd.concat([
+        test_df.assign(base_split="test"),
+        val_df.assign(base_split="validation"),
+        pool_df.assign(base_split="pool")
+    ])
+
+    base_data_split_save_path.parent.mkdir(parents=True, exist_ok=True)
+    base_data_split_df.to_csv(base_data_split_save_path, index=False)
 
 
-def split_csv_initial_training_set(pool_csv_path=POOL_SPLIT_SAVE_PATH, initial_training_size=1000, random_state=INITIAL_DATA_SPLIT_SEED, initial_train_split_save_path=INITIAL_TRAINING_SPLIT_SAVE_PATH, initial_remaining_pool_split_save_path=INITIAL_REMAINING_POOL_SPLIT_SAVE_PATH):
-    pool_df = pd.read_csv(pool_csv_path)
-    number_of_classes = pool_df["label"].nunique()
-    initial_training_df = pool_df.groupby("label", group_keys=False).sample(initial_training_size // number_of_classes, random_state=random_state)
 
-    remaining_pool_df = pool_df.drop(index=initial_training_df.index)
+def split_csv_initial_training_set(base_data_split_csv_path=BASE_DATA_SPLIT_CSV_PATH, initial_training_size=1000, random_state=INITIAL_DATA_SPLIT_SEED):
+    df = pd.read_csv(base_data_split_csv_path)
+    number_of_classes = df["label"].nunique()
 
-    initial_train_split_save_path.parent.mkdir(parents=True, exist_ok=True)
-    initial_remaining_pool_split_save_path.parent.mkdir(parents=True, exist_ok=True)
+    initial_training_split_indices = df[df['base_split']=="pool"].groupby("label", group_keys=False).sample(initial_training_size // number_of_classes, random_state=random_state).index
 
-    initial_training_df.to_csv(initial_train_split_save_path, index=False)
-    remaining_pool_df.to_csv(initial_remaining_pool_split_save_path, index=False)
+    df.loc[initial_training_split_indices, "base_split"] = "initial_train"
+    df.to_csv(base_data_split_csv_path, index=False)
 
 if __name__ == "__main__":
     split_csv_test_validation_pool()
