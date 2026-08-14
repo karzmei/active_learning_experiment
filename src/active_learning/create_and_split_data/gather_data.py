06c84aa37d61827c7259ed65e9704f7045620fc5
Karina Samvelyan <karina.samvelyan@gmail.com>
2026-08-14 16:49:04 +0200

    making the initial dataset order deterministic

--- src/active_learning/create_and_split_data/gather_data.py
+++ src/active_learning/create_and_split_data/gather_data.py
@@ -11,12 +11,15 @@ def download_data(data_dir_path: Path = DATA_MAIN_DIR_PATH):
     )
 
 def create_main_data_table(data_dir_path: Path = DATA_FOLDERS_PATH, path_to_save_csv: Path = DATA_MAIN_CSV_PATH):
-    classes_dir_paths = data_dir_path.iterdir()
+    classes_dir_paths = sorted(
+        path for path in data_dir_path.iterdir()
+        if path.is_dir()
+    )
 
     relative_paths = []
     labels = []
     for class_dir_path in classes_dir_paths:
-        img_paths = list(class_dir_path.glob("*.jpg"))
+        img_paths = sorted(class_dir_path.glob("*.jpg"))
         relative_img_paths = [img_path.relative_to(data_dir_path) for img_path in img_paths]
         relative_paths.extend(relative_img_paths)
         labels.extend([class_dir_path.stem] * len(relative_img_paths))
@@ -26,6 +29,7 @@ def create_main_data_table(data_dir_path: Path = DATA_FOLDERS_PATH, path_to_save
         DataSplitColNames.CLASS_NAME: labels
     })
 
+    path_to_save_csv.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(path_to_save_csv, index=False)
 
 
