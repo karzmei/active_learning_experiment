45cf35a6dfb08369c24a1eb8bb9dd16ba7384212
Karina Samvelyan <karina.samvelyan@gmail.com>
2026-08-14 15:45:22 +0200

    inheritance and other minor improvements

--- src/active_learning/model.py
+++ src/active_learning/model.py
@@ -1,18 +1,18 @@
 import torch
 from torchvision.models import resnet18, ResNet18_Weights
 
-class ResNet18Model:
+class ResNet18Model(torch.nn.Module):
     def __init__(self, pretrained=True, num_classes=10):
+        super().__init__()
+
         weights = ResNet18_Weights.DEFAULT if pretrained else None
         self.model = resnet18(weights=weights)
 
         last_layer_num_features = self.model.fc.in_features
-        self.classifier = torch.nn.Linear(last_layer_num_features, num_classes)
-        self.model.fc = self.classifier
+        self.model.fc = torch.nn.Linear(last_layer_num_features, num_classes)
 
     def forward(self, x):
-        output = self.model(x)
-        return output
+        return self.model(x)
 
     def predict(self, input_tensor):
         with torch.no_grad():
@@ -25,5 +25,5 @@ if __name__ == "__main__":
     print(model)
 
     sample_input = torch.randn(1, 3, 224, 224)
-    sample_output = model.forward(sample_input)
+    sample_output = model(sample_input)
     print(sample_output)
